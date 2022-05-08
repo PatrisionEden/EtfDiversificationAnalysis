897ae6d7ba93b126567bff101821d4b31c9d84e6
Andrey Hristoforov <andrey.hristoforov@gmail.com>
2022-05-08 12:30:27 +0400

    SomeCommit

---
+++ Server/Server/TockenMiddleware.cs
@@ -0,0 +1,55 @@
+﻿namespace Server
+{
+    public class TockenMiddleware
+    {
+        readonly RequestDelegate next;
+        //some
+
+        private List<string> PathToWatch = new List<string>()
+        {
+            "/saveinvestmentportfolio",
+            "/pizzamozarera",
+            "/rerarerarera"
+        };
+        public TockenMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(!PathToWatch.Contains(context.Request.Path))
+            {
+                await next.Invoke(context);
+                return;
+            }
+
+            if (!context.Request.Cookies.ContainsKey("tocken") || !context.Request.Cookies.ContainsKey("login"))
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("unauthorized");
+                return;
+            }
+
+            Authorisation authorisation = Authorisation.GetInstace();
+            var tocken = context.Request.Cookies["tocken"];
+            var login = context.Request.Cookies["login"];
+
+            if (tocken == null || login == null)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("tocken or login was null");
+                return;
+            }
+
+            if (!authorisation.IsThisTockenForThatLoginValid(login, tocken))
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync("tocken did not pass the authorization");
+                return;
+            }
+
+            await next.Invoke(context);
+            return;
+        }
+    }
+}
